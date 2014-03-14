4ee962978c3933b63c0192b717a99345adba01a9
congyuandong <congyuandong@gmail.com>
2014-03-14 15:21:57 +0800

    first commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '222.27.254.36';
+$cfg_dbname = 'dedecms_train';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '123';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
